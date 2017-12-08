678837e6b689eebc28f313aeb9e40b39796814b0
Alexandre Clement <alexandre.clement@etu.unice.fr>
2017-12-08 06:26:16 +0100

    Le chat bot ! v2.1

--- app/src/main/java/fr/unice/polytech/nuit_de_linfo_2017/chatbot/Wendy.java
+++ app/src/main/java/fr/unice/polytech/nuit_de_linfo_2017/chatbot/Wendy.java
@@ -12,10 +12,13 @@ public class Wendy
     public Wendy()
     {
         dialogs.add(new Dialog("Bonjour|Salut|Coucou", "Salut, comment ça va ?"));
-        dialogs.add(new Dialog("ça va ?", "Je vais bien et vous ?"));
-        dialogs.add(new Dialog("(?:nom|appel) ?", "Salut, je m'appel Wendy."));
+        dialogs.add(new Dialog("ça va \\?", "Je vais bien et vous ?"));
+        dialogs.add(new Dialog("ça va", "Super, en quoi puis-je vous aider ?"));
+        dialogs.add(new Dialog("(?:nom|appel) \\?", "Salut, je m'appel Wendy."));
         dialogs.add(new Dialog("Je le savais", "Tu ne sais rien Jhon Snow !"));
-        dialogs.add(new Dialog("Le saviez vous ?", "C'est un jeu excelent !"));
+        dialogs.add(new Dialog("Le saviez vous \\?", "C'est un jeu excelent !"));
+        dialogs.add(new Dialog("(?:faire|subir|victime).*?(?:accident) \\?", "Gardez votre calme, placer vous derrière la barrière de sécurité et attendez l'arrivée des secours."));
+        dialogs.add(new Dialog("(?:voir|assister).*?(?:accident)", "Rangez vous sur le coté si cela est possible afin d'appeler les secours."));
         dialogs.add(new Dialog("(?:résout|énigme|resout|enigme) (?:2|deux)", "Bravo, vous avez résolue la deuxième énigme."));
     }
 
